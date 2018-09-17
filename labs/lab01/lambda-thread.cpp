35fed3ae75ae94d7cca214c16123b584f2747925
NAPIER-MAIL\40206111 <40206111@live.napier.ac.uk>
2018-09-17 15:58:09 +0100

    Lab01

--- labs/lab01/lambda-thread.cpp
+++ labs/lab01/lambda-thread.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main(int argc, char **argv)
 {
     // Create a thread using a lambda expression
-    thread t([]{ cout << "Hello from lambda thread!" << endl; });
+    thread t([]{ cout << "Hello from lambda thread! from thread: " << this_thread::get_id() << endl; });
     // Join thread
     t.join();
 
