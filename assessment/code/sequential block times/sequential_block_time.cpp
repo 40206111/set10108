21fe3de1523f6c8741481818d09c281b4b1ddc40
Emma Parsley <40206111@live.napier.ac.uk>
2018-10-28 02:30:09 +0000

    parallel sha

--- assessment/code/sequential block times/sequential_block_time.cpp
+++ assessment/code/sequential block times/sequential_block_time.cpp
@@ -6,21 +6,23 @@ using namespace std;
 
 int main()
 {
-    block_chain bchain;
-	
-	//open file to hold data
-	bchain.file.open("sequential-5.csv");
 	for (uint32_t j = 0; j < 100u; ++j)
 	{
+		block_chain bchain;
+
+		//open file to hold data
+		bchain.file.open("sequential-5.csv", ios::app);
+
 		for (uint32_t i = 1; i < 11u; ++i)
 		{
 			cout << "Mining block " << i << "..." << endl;
 			bchain.add_block(block(i, string("Block ") + to_string(i) + string(" Data")));
 		}
 		bchain.file << endl;
+
+		//close file to hold data
+		bchain.file.close();
 	}
 
-	//close file to hold data
-	bchain.file.close();
     return 0;
 }
